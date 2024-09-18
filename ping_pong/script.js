ab7d0b3ad9f3292061f24ca4380fba4ea6c2a99a
cod1ngByte <rajiv.ranjansoc@gmail.com>
2024-09-18 16:01:39 +0530

    add collision of ball and ping-pong

--- ping_pong/script.js
+++ ping_pong/script.js
@@ -3,47 +3,42 @@ document.addEventListener("DOMContentLoaded", function () {
     const gameArena = document.querySelector(".game-arena");
     const ping = document.querySelector(".ping-pong");
     const ball = document.querySelector(".ball");
+    let py;
+    let dx;
+    let dy;
+    let id;
 
-    //initial value :
-    // console.log(gameArena.offsetWidth);
-    // console.log(gameArena.offsetHeight);
-    // console.log(ball.offsetWidth);
-    // console.log(ball.offsetHeight);
+    let ballDir = {};
+    let pingDir = {};
 
-    //displacement of ball in x and y direction
-    let dx = 2;
-    let dy = 2;
+    init();
 
-    //initial position of ball with respect to game-arena
-    ballDir = {
-        x: 350,
-        y: 225,
-    };
-    //initail position of table with respect to game-arena
-    pingDir = {
-        y: 180,
-    };
-
-    draw(ball, ballDir);
-    draw(ping, pingDir);
-
-    let id = setInterval(() => {
+    id = setInterval(() => {
         updateBallMov();
     }, 10);
 
     document.addEventListener("keydown", pingMovement);
 
-    function pingMovement(e) {
-        if (e.code === "ArrowUp" && pingDir.y > 0) {
-            pingDir.y = pingDir.y - 10;
-            draw(ping, pingDir);
-        } else if (
-            e.code === "ArrowDown" &&
-            pingDir.y < gameArena.offsetHeight - ping.offsetHeight - 5
-        ) {
-            pingDir.y = pingDir.y + 10;
-            draw(ping, pingDir);
-        }
+    function init() {
+        //displacemtn of paddle in y direction
+        py = 10;
+
+        //displacement of ball in x and y direction
+        dx = 2;
+        dy = 2;
+
+        //initial position of ball with respect to game-arena
+        ballDir = {
+            x: 350,
+            y: 225,
+        };
+        //initail position of table with respect to game-arena
+        pingDir = {
+            y: 180,
+        };
+
+        draw(ball, ballDir);
+        draw(ping, pingDir);
     }
 
     function draw(ele, eleDir) {
@@ -57,17 +52,49 @@ document.addEventListener("DOMContentLoaded", function () {
 
         draw(ball, ballDir);
 
+        //ball collison with paddle
+        if (
+            ballDir.x < ping.offsetLeft + ping.offsetWidth &&
+            ballDir.y > ping.offsetTop &&
+            ballDir.y < ping.offsetTop + ping.offsetHeight
+        ) {
+            dx = dx * -1;
+        }
+
+        //changing the direction of ball in x direction with respect to game arena
         if (
             ballDir.x > gameArena.offsetWidth - ball.offsetWidth ||
             ballDir.x <= 0
         ) {
             dx = dx * -1; //changing the direction of ball in x
         }
+
+        //changing the direction of ball in y direction with respect to game arena
         if (
             ballDir.y > gameArena.offsetHeight - ball.offsetHeight ||
             ballDir.y <= 0
         ) {
             dy = dy * -1; // changing the direction of ball in y
+        }
+    }
+
+    function collision() {
+        //  else {
+        //     clearInterval(id);
+        // }
+    }
+
+    function pingMovement(e) {
+        e.preventDefault();
+        if (e.code === "ArrowUp" && pingDir.y > 0) {
+            pingDir.y = pingDir.y + py * -1;
+            draw(ping, pingDir);
+        } else if (
+            e.code === "ArrowDown" &&
+            pingDir.y < gameArena.offsetHeight - ping.offsetHeight - 5
+        ) {
+            pingDir.y = pingDir.y + py;
+            draw(ping, pingDir);
         }
     }
 });
